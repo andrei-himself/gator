aa0d4495bfdaf660a205dfb2916f537a6d677ce9
andrei-himself <67463221+andrei-himself@users.noreply.github.com>
2025-11-04 13:21:11 +0200

    Finished config

--- main.go
+++ main.go
@@ -3,20 +3,73 @@ package main
 import (
 	"fmt"
 	"github.com/andrei-himself/gator/internal/config"
+	"os"
 )
 
-func main() {
-	cfg, err := config.Read()
-	if err != nil {
-		fmt.Errorf("%v\n", err)
-	} 
-	err = cfg.SetUser("andrei")
-	if err != nil {
-		fmt.Errorf("%v\n", err)
+type state struct {
+	p *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	m map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	com, ok := c.m[cmd.name]
+	if ok != true {
+		return fmt.Errorf(fmt.Sprintf("command '%v' not found", cmd.name))
 	}
-	cfg, err = config.Read()
+	return com(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.m[name] = f
+} 
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login command expects username as an argument")
+	}
+	err := s.p.SetUser(cmd.args[0])
 	if err != nil {
-		fmt.Errorf("%v\n", err)
+		return err
 	} 
-	fmt.Printf("%+v\n", cfg)
+	fmt.Println("User has been set!")
+	return nil
+}
+
+func main() {
+	var cfg state
+	conf, err := config.Read()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	} 
+	cfg.p = &conf
+
+	var commands commands
+	commands.m = map[string]func(*state, command) error{}
+	commands.register("login", handlerLogin)
+	args := os.Args
+	if len(args) < 2 {
+		err := fmt.Errorf("Not enough arguments")
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	name := args[1]
+	cmdArgs := args[2:]
+	cmd := command{
+		name: name,
+		args: cmdArgs,
+	}
+	err = commands.run(&cfg, cmd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
